eb4293cc530690910e02678044951f8cbf92548a
Esdras Antero <esdrasanterofontes@gmail.com>
2023-01-02 18:28:38 -0300

    fix: dump.sql createdAt

--- src/pages/ranking/ranking.js
+++ src/pages/ranking/ranking.js
@@ -3,8 +3,19 @@ import styled from "styled-components"
 
 export default function RankingPage () {
     return (
-        <div>oi</div>
+        <Container>
+            <Head
+        </Container>
     )
 }
 
 //Styled Components
+const Container = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px;
+`
